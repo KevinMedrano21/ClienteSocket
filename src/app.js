a5c846746ab349ab6c5cf76471d5a780d6c16b65
kevinmedrano21 <kmedrano433@gmail.com>
2023-10-31 17:32:15 -0600

    adaptacion

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const IndexRoutes = require('./routers/index.js');
 const { default: mongoose } = require('mongoose');
 
 
-app.set('port', process.env.SERVER_URL || '127.0.0.1:3000'); // asignacion de puerto
+app.set('port', process.env.SERVER_URL); // asignacion de puerto
 app.set('view engine', 'ejs');
 
 //MiddleWare utiliza morgan
