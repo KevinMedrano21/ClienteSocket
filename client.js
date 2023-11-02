faf07bdb66670555e10bbbd04e0613448a5da6db
kevinmedrano21 <kmedrano433@gmail.com>
2023-11-01 19:08:18 -0600

    prueba de puerto6

--- client.js
+++ client.js
@@ -8,47 +8,33 @@ const readline = require('readline-sync');
 //     port: process.env.SERVER_PORT // Utiliza una variable de entorno para el puerto
 // };
 
-function connectToServer(){
+function connectToServer() {
     const client = new net.Socket();
-}
 
+    client.connect(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
+        console.log('Conexión satisfactoria');
+        sendLine(client);
+    });
 
-client.connect(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
-    console.log('Conexión satisfactoria');
-    sendLine(client);
-});
+    client.on('error', (err) => {
+        console.error(err.message);
+        // Si hay un error, intenta la reconexión después de un tiempo
+        setTimeout(connectToServer, 1000);
+    });
 
-// const client = net.createConnection(servidor);
+    client.on('data', (data) => {
+        console.log('El servidor dice ' + data);
+        sendLine(client);
+    });
 
-// client.on('connect', ()=>{
-//     console.log('conexion satisfactoria')
-//     sendLine(cliente)
-// })
-
-client.on('error', (err) => {
-    console.error(err.message);
-    // Si hay un error, intenta la reconexión después de un tiempo
-    setTimeout(connectToServer, 1000);
-});
-
-client.on('data', (data)=>{
-    console.log('El servidor dice' +data)
-    sendLine()
-})
-
-client.on('error', (err)=>{
-    console.log(err.message)
-})
-
-
-function sendLine(){
-    var line = readline.question('\n ingresa un mensaje \n')
-    if (line==0) {
-        client.end()
-    }else{
-        client.write(line)
+    function sendLine(client) {
+        const line = readline.question('\nIngresa un mensaje\n');
+        if (line === '0') {
+            client.end();
+        } else {
+            client.write(line);
+        }
     }
 }
 
 connectToServer();
-
